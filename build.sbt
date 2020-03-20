06f9ab7d2432443a4faaa9caabdf9bc0e3d69640
Daehyun Kim <hatemogi@gmail.com>
2020-03-21 00:23:35 +0900

    Finagle 서비스 코드 추가

--- build.sbt
+++ build.sbt
@@ -5,5 +5,9 @@ scalaVersion := "2.13.1"
 lazy val root = (project in file("."))
   .settings(
     name := "scala-backend",
-    libraryDependencies += "org.scalatest" %% "scalatest" % "3.1.1" % Test
+    libraryDependencies ++= Seq(
+      "com.twitter" %% "finagle-http" % "20.3.0",
+      "ch.qos.logback" % "logback-classic" % "1.2.3",
+      "org.scalatest" %% "scalatest" % "3.1.1" % Test
+    )
   )
--- src/main/scala/com/hatemogi/scala/Main.scala
+++ src/main/scala/com/hatemogi/scala/Main.scala
@@ -1,8 +1,21 @@
 package com.hatemogi.scala
 
+import com.twitter.finagle.{Http, Service, http}
+import com.twitter.util.{Await, Future}
+
 object Main extends App {
   def factorial(n: Int): Int =
     (1 to n).product
 
   println("factorial(5) = " + factorial(5))
+
+  val service = new Service[http.Request, http.Response] {
+    def apply(req: http.Request): Future[http.Response] =
+      Future.value(
+        http.Response(req.version, http.Status.Ok)
+      )
+  }
+
+  val server = Http.serve(":8080", service)
+  Await.ready(server)
 }
