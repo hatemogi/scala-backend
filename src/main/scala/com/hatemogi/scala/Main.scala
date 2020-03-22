1897264ba8cadb12adbf8668361699606be03401
Daehyun Kim <hatemogi@gmail.com>
2020-03-22 22:38:24 +0900

    Finagle 응답에 본문 추가

--- src/main/scala/com/hatemogi/scala/Main.scala
+++ src/main/scala/com/hatemogi/scala/Main.scala
@@ -1,6 +1,7 @@
 package com.hatemogi.scala
 
 import com.twitter.finagle.{Http, Service, http}
+import com.twitter.io.Buf
 import com.twitter.util.{Await, Future}
 
 object Main extends App {
@@ -12,8 +13,9 @@ object Main extends App {
   val service = new Service[http.Request, http.Response] {
     def apply(req: http.Request): Future[http.Response] =
       Future.value(
-        http.Response(req.version, http.Status.Ok)
-      )
+        http.Response(http.Version.Http11, http.Status.Ok)
+          .content(Buf.Utf8("Hello"))
+    )
   }
 
   val server = Http.serve(":8080", service)
