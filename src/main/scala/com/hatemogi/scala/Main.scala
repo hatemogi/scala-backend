ff576449664d24d071e413e919c42fdb2ce5d08e
Daehyun Kim <hatemogi@gmail.com>
2020-03-22 22:47:54 +0900

    Finagle 응답에 헤더 추가

--- src/main/scala/com/hatemogi/scala/Main.scala
+++ src/main/scala/com/hatemogi/scala/Main.scala
@@ -10,12 +10,11 @@ object Main extends App {
 
   println("factorial(5) = " + factorial(5))
 
-  val service = new Service[http.Request, http.Response] {
-    def apply(req: http.Request): Future[http.Response] =
-      Future.value(
-        http.Response(http.Version.Http11, http.Status.Ok)
-          .content(Buf.Utf8("Hello"))
-    )
+  val service: Service[http.Request, http.Response] = { req =>
+    val res = http.Response(req.version, http.Status.Ok)
+    res.content(Buf.Utf8("안녕하세요"))
+    res.setContentType("text/plain", "UTF-8")
+    Future.value(res)
   }
 
   val server = Http.serve(":8080", service)
