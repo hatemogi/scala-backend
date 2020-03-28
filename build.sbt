961243831b298edb513edfe6353600f06b74a05d
Daehyun Kim <hatemogi@gmail.com>
2020-03-28 22:59:24 +0900

    Finagle대신 http4s 사용하기로

--- build.sbt
+++ build.sbt
@@ -2,12 +2,34 @@ version := "0.1"
 
 scalaVersion := "2.13.1"
 
+val http4sVersion = "0.21.2"
+val circeVersion = "0.13.0"
+val catsVersion = "2.0.0"
+
+val http4sDependencies = Seq(
+  "org.http4s" %% "http4s-dsl",
+  "org.http4s" %% "http4s-blaze-server",
+  "org.http4s" %% "http4s-blaze-client",
+  "org.http4s" %% "http4s-circe"
+).map(_ % http4sVersion)
+
+val jsonDependencies = Seq(
+  "io.circe" %% "circe-core",
+  "io.circe" %% "circe-generic",
+  "io.circe" %% "circe-parser"
+).map(_ % circeVersion)
+
+val catsDependencies = Seq(
+  "org.typelevel" %% "cats-core",
+  "org.typelevel" %% "cats-effect"
+).map(_ % catsVersion)
+
 lazy val root = (project in file("."))
   .settings(
     name := "scala-backend",
     libraryDependencies ++= Seq(
-      "com.twitter" %% "finagle-http" % "20.3.0",
       "ch.qos.logback" % "logback-classic" % "1.2.3",
-      "org.scalatest" %% "scalatest" % "3.1.1" % Test
-    )
+      "org.scalatest" %% "scalatest" % "3.1.1" % Test,
+      "org.scalacheck" %% "scalacheck" % "1.14.1" % Test
+    ) ++ http4sDependencies ++ jsonDependencies ++ catsDependencies
   )
--- src/main/scala/com/hatemogi/scala/Main.scala
+++ src/main/scala/com/hatemogi/scala/Main.scala
@@ -1,8 +1,14 @@
 package com.hatemogi.scala
 
-import com.twitter.finagle.{Http, Service, http}
-import com.twitter.io.Buf
-import com.twitter.util.{Await, Future}
+import cats.effect._
+import cats.implicits._
+import org.http4s._
+import org.http4s.dsl.io._
+import org.http4s.implicits._
+import org.http4s.server.Router
+import org.http4s.server.blaze._
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Main extends App {
   def factorial(n: Int): Int =
@@ -10,13 +16,23 @@ object Main extends App {
 
   println("factorial(5) = " + factorial(5))
 
-  val service: Service[http.Request, http.Response] = { req =>
-    val res = http.Response(req.version, http.Status.Ok)
-    res.content(Buf.Utf8("안녕하세요"))
-    res.setContentType("text/plain", "UTF-8")
-    Future.value(res)
+  implicit val cs: ContextShift[IO] = IO.contextShift(global)
+  implicit val timer: Timer[IO] = IO.timer(global)
+
+  val helloWorldService = HttpRoutes.of[IO] {
+    case GET -> Root / "hello" / name =>
+      Ok(s"안녕하세요, $name.")
   }
 
-  val server = Http.serve(":8080", service)
-  Await.ready(server)
+  val httpApp = Router("/" -> helloWorldService).orNotFound
+
+  val server = BlazeServerBuilder[IO]
+    .bindHttp(8080, "localhost")
+    .withHttpApp(httpApp)
+    .serve
+    .compile
+    .drain
+    .as(ExitCode.Success)
+
+  server.unsafeRunSync()
 }
